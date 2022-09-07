4b2aba98175cc4171000481de7bb41f5791d6245
AsafShon <asaf.shon@gmail.com>
2022-09-07 14:41:37 +0300

    Transmission Spectrum completed = v1.0.0

--- Algo/TransmissionSpectrum.py
+++ Algo/TransmissionSpectrum.py
@@ -17,35 +17,45 @@ import time
 
 
 
-import BasicInstrumentsControl.Laser.LaserControl as Laser
+from BasicInstrumentsControl.PicoControl.PicoControl import PicoControl as Pico
 from BasicInstrumentsControl.PicoControl.PicoControl import PicoScopeControl as Scope
 from BasicInstrumentsControl.PicoControl.PicoControl import PicoSigGenControl as SigGen
+from BasicInstrumentsControl.Laser.LaserControl import LaserControl as Laser
 
 # PARAMETERS
 WAIT_TIME = 2
 
 
 class TransmissionSpectrum:
-    def __init__(self,directory='20220824-0002',init_wavelength = 776e-9,final_wavelength = 781e-9,Python_Control = True):
+    def __init__(self,directory='20220824-0002',init_wavelength = 776,final_wavelength = 781,Python_Control = True):
         """"""
         if Python_Control:
-            self.SigGen = SigGen(pk_to_pk_voltage = 2, offset_voltage = 0.0, frequency = 10,wave_type = 'RAMP_UP')
+            self.Pico = Pico()
+            self.SigGen = SigGen(pico=self.Pico,pk_to_pk_voltage = 2, offset_voltage = 0, frequency = 10,wave_type = 'RAMP_UP')
+            self.Scope = Scope(pico=self.Pico)
+            self.Laser = Laser()
+
+            self.final_wavelength = final_wavelength
+            self.init_wavelength = init_wavelength
             self.single_scan_width = self.SigGen.calculate_scan_width()
-            for i in np.arange(init_wavelength, final_wavelength, self.single_scan_width):
-                Laser.tlb_set_wavelength(i)
+
+            self.partial_spectrum = []
+            for i in np.arange(self.init_wavelength, self.final_wavelength, self.single_scan_width):
+                self.Laser.tlb_set_wavelength(i)
                 time.sleep(WAIT_TIME)
-                self.spectrum_sheet.append(Scope.get_trace[0])
+                self.partial_spectrum.append(self.Scope.get_trace()[0])
+            self.total_spectrum = np.concatenate(self.partial_spectrum)
         else:
             self.directory = directory
-            self.spectrum_parts = []
+            self.partial_spectrum = []
             self.total_spectrum = []
             for i in range(1, 1500, 50):
                 filename = '20220824-0001_' + '{0:04}'.format(i) + '.csv'
                 full_path = self.directory+'//'+ filename
-                self.spectrum_parts.append(self.read_csv(full_path)[2:])
+                self.partial_spectrum.append(self.read_csv(full_path)[2:])
 
-        self.total_spectrum = np.concatenate(self.spectrum_parts)
-        self.total_spectrum = [item[1] for item in self.total_spectrum]
+            self.total_spectrum = np.concatenate(self.partial_spectrum)
+            self.total_spectrum = [item[1] for item in self.total_spectrum]
 
         self.total_spectrum = [float(item) for item in self.total_spectrum]
         self.total_spectrum = np.array(self.total_spectrum)
@@ -83,10 +93,8 @@ class TransmissionSpectrum:
         plt.show()
 
     def plot_unfied_spectrum(self):
-        wave_number_init = 776.5
-        wave_number_final = 781
-        m_wavenumber_transmitted = (wave_number_final-wave_number_init)/len(self.total_spectrum)
-        wavenumber_transmitted = m_wavenumber_transmitted*np.arange(0,len(self.total_spectrum))+wave_number_init
+        m_wavenumber_transmitted = (self.final_wavelength-self.init_wavelength)/len(self.total_spectrum)
+        wavenumber_transmitted = m_wavenumber_transmitted*np.arange(0,len(self.total_spectrum))+self.init_wavelength
         plt.plot(wavenumber_transmitted,self.total_spectrum)
         plt.show()
 
@@ -94,7 +102,9 @@ class TransmissionSpectrum:
         plt.savefig(dist_name)
 
 if __name__ == "__main__":
-    o=TransmissionSpectrum(init_wavelength = 776e-9,final_wavelength = 781e-9,Python_Control = True)
+    o=TransmissionSpectrum(init_wavelength = 776,final_wavelength = 781,Python_Control = True)
+    o.plot_unfied_spectrum()
+    o.Pico.__del__()
     # scan_spectrum = []
     # for i in range (2,6):
     #     scan_spectrum += [o.piezo_scan_spectrum(i)]
