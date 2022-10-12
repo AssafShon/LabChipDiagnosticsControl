023d5bdd4c778c991eb40ed30ff55066e7bf7851
AssafShon <asaf.shon@gmail.com>
2022-10-12 14:59:09 +0300

    Add save_spectrum with date

--- Algo/TransmissionSpectrum.py
+++ Algo/TransmissionSpectrum.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 from scipy import signal
-
+from datetime import datetime
 
 
 from BasicInstrumentsControl.PicoControl.PicoControl import PicoControl as Pico
@@ -38,12 +38,14 @@ class TransmissionSpectrum:
             self.final_wavelength = final_wavelength
             self.init_wavelength = init_wavelength
             self.single_scan_width = self.SigGen.calculate_scan_width()
-            time.sleep(WAIT_TIME)
+            self.Laser.tlb_set_wavelength(self.init_wavelength)
+            time.sleep(5*WAIT_TIME)
 
             self.partial_spectrum = []
             for i in np.arange(self.init_wavelength, self.final_wavelength, self.single_scan_width):
                 self.Laser.tlb_set_wavelength(i)
                 time.sleep(WAIT_TIME)
+
                 self.partial_spectrum.append(self.Scope.get_trace()[1])
             self.total_spectrum = np.concatenate(self.partial_spectrum)
         else:
@@ -93,29 +95,31 @@ class TransmissionSpectrum:
     #         ax.plot(Y)
     #     plt.show()
 
-    def plot_unfied_spectrum(self,filter_order=4, filter_critical_freq=0.99):
-        #filter the signal
-        b, a = signal.butter(filter_order, filter_critical_freq)
-
+    def filter_spectrum(self, filter_type = 'high',filter_order=4, filter_critical_freq=0.9):
+        b, a = signal.butter(filter_order, filter_critical_freq, btype=filter_type)
+        return signal.filtfilt(b, a, self.total_spectrum)
+    def plot_spectrum(self, Y):
         m_wavenumber_transmitted = (self.final_wavelength-self.init_wavelength+self.single_scan_width)/len(self.total_spectrum)
         wavenumber_transmitted = m_wavenumber_transmitted*np.arange(0,len(self.total_spectrum))+(self.init_wavelength-self.single_scan_width/2)
+
 
         plt.figure()
         plt.title('Tansmission Spectrum')
         plt.xlabel('Wavelength[nm]')
         plt.ylabel('Voltage[mV]')
         plt.grid(True)
-        plt.plot(wavenumber_transmitted,signal.filtfilt(b, a, self.total_spectrum),'r')
+        plt.plot(wavenumber_transmitted, Y,'r')
         plt.show()
 
     def save_figure(self,dist_name):
-        plt.savefig(dist_name)
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        plt.savefig(timestr+'_'+dist_name)
 
 if __name__ == "__main__":
     try:
-        o=TransmissionSpectrum(init_wavelength = 770,final_wavelength = 781,Python_Control = True)
-        o.plot_unfied_spectrum(filter_critical_freq=0.99)
-        # o.save_figure()
+        o=TransmissionSpectrum(init_wavelength = 774,final_wavelength = 781,Python_Control = True)
+        o.plot_spectrum(o.total_spectrum)
+        o.save_figure('transmission_spectrum.png')
         o.Pico.__del__()
         o.Laser.__del__()
     except:
