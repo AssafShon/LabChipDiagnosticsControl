1659586d3be716cda4ec6bad44c54c13ac617f2d
assafs <asaf.shon@gmail.com>
2022-11-29 10:18:47 +0200

    AnalyzeSpectrum.py - added, still not functioning well, need to work on the fit to lorentzians

---
+++ Algo/SpectrumAnalysis/AnalyzeSpectrum.py
@@ -0,0 +1,78 @@
+
+from scipy.interpolate import CubicSpline
+from scipy.signal import peak_widths, find_peaks
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+#parameters
+C_light= 2.99792458e8
+
+class AnalyzeSpectrum():
+    def __init__(self, decimation = 1000, prominence=20, height=None, distance=None, rel_height=0.5,run_experiment=False):
+        if run_experiment:
+            pass
+        else:
+            data = np.load(
+                r'C:\Users\asafs\qs-labs\R&D - Lab\Chip Tester\Spectrum_transmission\20221123-103148Transmission_spectrum.npz')
+            self.total_spectrum = data['spectrum']
+            self.scan_wavelengths = data['wavelengths']
+        self.smooth_spectrum(decimation)
+        self.peaks_width,self.peaks = self.find_peaks_in_spectrum(prominence,height,distance,rel_height)
+        #plot figure with peaks
+        plt.figure()
+        plt.plot(self.interpulated_spectrum)
+        plt.plot(self.peaks,self.interpulated_spectrum[self.peaks],'o')
+        plt.hlines(*self.peaks_width[1:])
+
+        self.lorenzians_width = self.fit_lorenzians()
+
+        plt.figure()
+        plt.plot(self.scan_freqs,self.Lorenzian(self.scan_freqs, *self.popt), 'r-')
+        plt.show()
+
+    def smooth_spectrum(self,decimation):
+        '''
+        smooth the spectrum transmission with decimation and interpulation.
+        :return:
+        '''
+        self.decimated_total_spectrum = self.total_spectrum[0:-1:decimation]
+        self.decimated_scanned_wavelengths= self.scan_wavelengths[0:-1:decimation]
+
+        cs = CubicSpline(self.decimated_scanned_wavelengths, self.decimated_total_spectrum)
+        self.interpulated_spectrum = cs(self.scan_wavelengths)
+
+    def find_peaks_in_spectrum(self,prominence,height,distance,rel_height):
+        '''
+        find peaks in transmission spectrum
+        :param prominence: The prominence of a peak measures how much a peak stands out from the surrounding
+                           baseline of the signal and is defined as the vertical distance between the peak and its lowest contour line.
+        :return:
+        '''
+        peaks, _ = find_peaks(-self.interpulated_spectrum, prominence=prominence,height=height,distance=distance)
+        peaks_width = peak_widths(-self.interpulated_spectrum, peaks, rel_height=rel_height)
+        peaks_width =(peaks_width[0],-peaks_width[1],peaks_width[2],peaks_width[3])
+        return peaks_width,peaks
+
+    def fit_lorenzians(self):
+        #the frequencies of the scan obtained from wavelengths
+        self.scan_freqs = self.get_scan_freqs()
+        for i in range(len(self.peaks)):
+            initial_guess = np.array([self.peaks_width[0][i]/2, self.peaks_width[0][i]/2, self.peaks[i],0])
+            self.popt[i], pcov = curve_fit(self.Lorenzian, self.scan_freqs[(self.peaks[i] - int(self.peaks_width[0][i])):(self.peaks[i] + int(self.peaks_width[0][i]))],
+                                   self.interpulated_spectrum[(self.peaks[i] - int(self.peaks_width[0][i])):(self.peaks[i] + int(self.peaks_width[0][i]))],
+                               bounds=([0, 0, -200000,0], [1e10, 1e4, 200000,1e3]), p0=initial_guess)
+
+    def get_scan_freqs(self):
+        '''
+
+        :return: freqs - in Mhz
+        '''
+        freqs = (C_light* 1e-6) / (self.scan_wavelengths * 1e-9)
+        return freqs
+
+    def Lorenzian(self,x, kex, ki, dc, h):
+        return (abs(1 - 2 * kex * (1j * (x - dc) + (kex + ki)) / (h ** 2 + (1j * (x - dc) + (kex + ki)) ** 2)) ** 2)
+
+if __name__ == "__main__":
+    o=AnalyzeSpectrum(decimation=10)
--- Algo/SpectrumAnalysis/TransmissionSpectrum.py
+++ Algo/SpectrumAnalysis/TransmissionSpectrum.py
@@ -17,7 +17,7 @@ import time
 from scipy import signal
 import os
 
-
+from AnalyzeSpectrum import AnalyzeSpectrum
 from BasicInstrumentsControl.PicoControl.PicoControl import PicoControl as Pico
 from BasicInstrumentsControl.PicoControl.PicoControl import PicoScopeControl as Scope
 from BasicInstrumentsControl.PicoControl.PicoControl import PicoSigGenControl as SigGen
@@ -31,7 +31,15 @@ CH_B = 1
 
 
 class TransmissionSpectrum:
-    def __init__(self,directory='20220824-0002',init_wavelength = 772,final_wavelength = 781,Python_Control = True, parmeters_by_console = True):
+    def __init__(self,directory='20220824-0002',init_wavelength = 772,final_wavelength = 781,Python_Control = True,parmeters_by_console = True):
+        '''
+
+        :param directory: directory to load traces (relevant when Python_Control=False)
+        :param init_wavelength: initial wavelength for scan [nm]
+        :param final_wavelength:
+        :param Python_Control:
+        :param decimation:
+        '''
         if Python_Control:
             self.Pico = Pico()
             self.SigGen = SigGen(pico=self.Pico,pk_to_pk_voltage = 0.8, offset_voltage = 0, frequency = 10,wave_type = 'TRIANGLE')
@@ -39,9 +47,9 @@ class TransmissionSpectrum:
             self.Laser = Laser()
 
             if parmeters_by_console:
-                print("Enter initial wavelength for scan:")
+                print("Enter initial wavelength for scan in [nm]:")
                 init_wavelength = float(input())
-                print("Enter final wavelength for scan:")
+                print("Enter final wavelength for scan in [nm]:")
                 final_wavelength = float(input())
 
             self.final_wavelength = final_wavelength
@@ -74,18 +82,21 @@ class TransmissionSpectrum:
 
             self.total_spectrum = np.concatenate(self.partial_spectrum)
             self.total_spectrum = [item[1] for item in self.total_spectrum]
-
+        #turn into numpy array of floats
         self.total_spectrum = [float(item) for item in self.total_spectrum]
         self.total_spectrum = np.array(self.total_spectrum)
 
+        # create vector of wavelengths in the scan
+        self.find_wavelength_scanned()
 
-    def piezo_scan_spectrum(self, i):
-        filename = '20220824-0001_' + '{0:04}'.format(i) + '.csv'
-        full_path = self.directory + '//' + filename
-        self.piezo_scan = self.read_csv(full_path)[2:]
-        self.piezo_scan = [item[1] for item in self.piezo_scan]
-        self.piezo_scan = [float(item) for item in self.piezo_scan]
-        return self.piezo_scan
+        self.analyzed_spectrum = AnalyzeSpectrum(decimation)
+
+
+    def find_wavelength_scanned(self):
+        m_wavenumber_transmitted = (self.final_wavelength - self.init_wavelength + self.single_scan_width) / len(
+            self.total_spectrum)
+        self.scan_wavelengths = m_wavenumber_transmitted * np.arange(0, len(self.total_spectrum)) + (
+                    self.init_wavelength - self.single_scan_width / 2)
 
     def read_csv(self, filename):
         csv_data = pd.read_csv(filename, sep=',', header=None)
@@ -96,9 +107,6 @@ class TransmissionSpectrum:
         return signal.filtfilt(b, a, self.total_spectrum)
 
     def plot_spectrum(self, Y):
-        m_wavenumber_transmitted = (self.final_wavelength-self.init_wavelength+self.single_scan_width)/len(self.total_spectrum)
-        self.scan_wavelengths = m_wavenumber_transmitted*np.arange(0,len(self.total_spectrum))+(self.init_wavelength-self.single_scan_width/2)
-
         plt.figure()
         plt.title('Tansmission Spectrum')
         plt.xlabel('Wavelength[nm]')
@@ -115,15 +123,15 @@ class TransmissionSpectrum:
         # save figure
         plt.savefig(os.path.join(directory_path,timestr+filename+'.png'))
         #save data as csv
-        np.savetxt(os.path.join(directory_path,timestr+filename+'.csv'), spectrum_data[0:-1:decimation], delimiter=',')
+        np.savetxt(os.path.join(directory_path,timestr+filename+'.csv'), spectrum_data, delimiter=',')
         #save python data
         np.savez(os.path.join(directory_path,timestr+filename+'.npz'), spectrum = spectrum_data[0:-1:decimation],wavelengths = self.scan_wavelengths[0:-1:decimation])
 
 if __name__ == "__main__":
     try:
-        o=TransmissionSpectrum(init_wavelength = 772,final_wavelength = 781,Python_Control = True)
-        # o.plot_spectrum(o.total_spectrum)
-        o.save_figure_and_data(r'C:\Users\Lab2\qs-labs\R&D - Lab\Chip Tester\Spectrum_transmission',[1,2,3],1000, 'Test')
+        o=TransmissionSpectrum(init_wavelength = 772,final_wavelength = 781,Python_Control = True, decimation = 1000)
+        o.plot_spectrum(o.total_spectrum)
+        o.save_figure_and_data(r'C:\Users\Lab2\qs-labs\R&D - Lab\Chip Tester\Spectrum_transmission',o.decimated_total_spectrum,1000, 'Test')
         o.Pico.__del__()
         o.Laser.__del__()
     except:
