5aa6144392ae612bcfc019f712ed51136cb89f34
assafs <asaf.shon@gmail.com>
2022-12-29 14:29:27 +0200

    AnalyzeSpectrum.py - added classification of peaks to different rings

--- Utility_functions.py
+++ Utility_functions.py
@@ -9,3 +9,14 @@ def plot_figure(X,Y,xlabel,ylabel,title):
     plt.title(title)
     plt.grid(True)
     plt.show()
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
